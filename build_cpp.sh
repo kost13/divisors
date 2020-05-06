1f5a93189f95686553ac52d22d536c66862396c5
lukasz <l.kostrzewa12@gmail.com>
2020-05-06 23:35:39 +0200

    refactor cmake

--- build_cpp.sh
+++ build_cpp.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+#if empty default python is used
+PYTHON_INCLUDE_PATH="/home/lukasz/miniconda3/envs/django_env/include/python3.6m"
+PYTHON_LIB_PATH="/home/lukasz/miniconda3/envs/django_env/lib/libpython3.6m.so"
+
 mkdir -p build && cd build
-cmake ../cpp/
+if [ -z "$PYTHON_INCLUDE_PATH" ] || [ -z "$PYTHON_LIB_PATH" ] ; then
+    echo "a"
+    cmake ../cpp/
+else
+    echo "b"
+    cmake ../cpp/ -DPYTHON_INCLUDE_DIRS=${PYTHON_INCLUDE_PATH} -DPYTHON_LIBRARIES=${PYTHON_LIB_PATH}
+fi
+
 cmake --build .
 cp libdivisors.so ../bin/divisors.so
 cd ..
